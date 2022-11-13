f0ed17cd84c2c3616ecb0f3abe0967693354ae06
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-11-13 23:33:12 +0530

    Selecting songs from library, skiping back and foward etc

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,40 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({
+  song,
+  songs,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    const selectedSong = songs.filter((state) => state.id === id);
+    await setCurrentSong(selectedSong[0]);
+    // Add Active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    // check if the song is playing
+    if (isPlaying) audioRef.current.play();
+  };
   return (
-    <div className='library-song'>
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
       <img alt={song.name} src={song.cover}></img>
       <div className='song-description'>
         <h3>{song.name}</h3>
--- src/data.js
+++ src/data.js
@@ -3,6 +3,16 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
+      name: "The Flash before my Eyes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41700",
+      color: ["#FFFFFF", "#E5B0B7"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "Joy Boy",
       cover:
         "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
@@ -13,12 +23,22 @@ function chillHop() {
       active: true,
     },
     {
-      name: "Just up",
+      name: "One Eye Open",
       cover:
-        "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
-      artist: "less.people",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=47640",
-      color: ["#EAE6D0", "#544645"],
+        "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
+      artist: "Allem Iversom, little blue",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=42241",
+      color: ["#EAEBDC", "#735234"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Awaken",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41653",
+      color: ["#FDF2D2", "#B95852"],
       id: uuidv4(),
       active: false,
     },
@@ -33,16 +53,6 @@ function chillHop() {
       active: false,
     },
     {
-      name: "One Eye Open",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
-      artist: "Allem Iversom, little blue",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=42241",
-      color: ["#EAEBDC", "#735234"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
       name: "Small Whispers",
       cover:
         "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
@@ -53,21 +63,21 @@ function chillHop() {
       active: false,
     },
     {
+      name: "Memories of When....",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41693",
+      color: ["#FFFFFF", "#E5B0B7"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "Skipping Stones",
       cover:
         "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
       artist: "Knowmadic",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=41655",
-      color: ["#FDF2D2", "#B95852"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Awaken",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
-      artist: "Knowmadic",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=41653",
       color: ["#FDF2D2", "#B95852"],
       id: uuidv4(),
       active: false,
@@ -93,22 +103,12 @@ function chillHop() {
       active: false,
     },
     {
-      name: "The Flash before my Eyes",
+      name: "Just up",
       cover:
-        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
-      artist: "Shrimpnose",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=41700",
-      color: ["#FFFFFF", "#E5B0B7"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Memories of When....",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
-      artist: "Shrimpnose",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=41693",
-      color: ["#FFFFFF", "#E5B0B7"],
+        "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
+      artist: "less.people",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=47640",
+      color: ["#EAE6D0", "#544645"],
       id: uuidv4(),
       active: false,
     },
