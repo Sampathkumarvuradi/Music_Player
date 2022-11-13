cc321537f19c6a0f197485a1dafeebc8840c8a4e
Sampath Kumar <sampathkumarvuradi@gmail.com>
2022-11-13 16:56:25 +0530

    Added music play pause feature & duration

---
+++ src/util.js
@@ -0,0 +1,118 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Joy Boy",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
+      artist: "less.people",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=47639",
+      color: ["#EAE6D0", "#544645"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Just up",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
+      artist: "less.people",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=47640",
+      color: ["#EAE6D0", "#544645"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Roses along the Way",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/41619d032f80a089e2c99e98953d441fd061ef12-1024x1024.jpg",
+      artist: "less.people",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=47642",
+      color: ["#EAE6D0", "#544645"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "One Eye Open",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
+      artist: "Allem Iversom, little blue",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=42241",
+      color: ["#EAEBDC", "#735234"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Small Whispers",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/10/d2fc5032585b1d577f2bd59d5dba4766d99884f6-1024x1024.jpg",
+      artist: "Allem Iversom, little blue",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=45071",
+      color: ["#EAEBDC", "#735234"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Skipping Stones",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41655",
+      color: ["#FDF2D2", "#B95852"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Awaken",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41653",
+      color: ["#FDF2D2", "#B95852"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Cycles",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/06/312458b974ab45c795d75ec3f0259caa3989103a-1024x1024.jpg",
+      artist: "Knowmadic",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41649",
+      color: ["#FDF2D2", "#B95852"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "In Minutes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41689",
+      color: ["#FFFFFF", "#E5B0B7"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "The Flash before my Eyes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41700",
+      color: ["#FFFFFF", "#E5B0B7"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Memories of When....",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/07/49bc1653b33bd42e570ecdeba7be86e8bd1b5c38-1024x1024.jpg",
+      artist: "Shrimpnose",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=41693",
+      color: ["#FFFFFF", "#E5B0B7"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
